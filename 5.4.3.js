aeb88953020355718d833ea033f7bb7a6684c657
wladbader <124190041+wladbader@users.noreply.github.com>
2023-04-04 13:02:37 +0600

    Update 5.4.3.js

--- 5.4.3.js
+++ 5.4.3.js
@@ -1,3 +1,5 @@
+//Дана строка. Необходимо вывести в консоль перевёрнутый вариант. Например, "Hello" -> "olleH".
+
 function stringReverse(string) {
     let arr = string.split("").reverse().join("");
     return arr;
