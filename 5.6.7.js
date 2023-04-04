92b2a75c8d55909a18af549d3418f27c554dc36d
wladbader <124190041+wladbader@users.noreply.github.com>
2023-04-04 12:48:48 +0600

    Update 5.6.7.js

--- 5.6.7.js
+++ 5.6.7.js
@@ -1,8 +1,8 @@
 let arr = [1, 1, 4, 7, 4, 3, null, 'abc', 14];
 let countEven = 0, countOdd = 0, countZero = 0;
 arr.map(item => {
-    +item % 2 == 0 ? countEven += 1 : countOdd += 1;
     +item === 0 ? countZero += 1 : countZero;
+    +item % 2 == 0 ? countEven += 1 : countOdd += 1;
 });
 
 console.log(`
