2626337bba99bdf068ec9e28c50006430b3a7a2d
wladbader <124190041+wladbader@users.noreply.github.com>
2023-04-04 12:58:51 +0600

    Update 5.6.7.js

--- 5.6.7.js
+++ 5.6.7.js
@@ -1,9 +1,16 @@
-let arr = [1, 1, 4, 7, 4, 3, null, 'abc', 14];
+let arr = [1, 1, 4, 7, 4, 3, null, 'abc', 14, 0];
 let countEven = 0, countOdd = 0, countZero = 0;
-arr.map(item => {
-    +item === 0 ? countZero += 1 : countZero;
-    +item % 2 == 0 ? countEven += 1 : countOdd += 1;
-});
+arr.map(
+    item => {
+        if (+item == 0) {
+            countZero += 1;
+        } else if (+item % 2 == 0) {
+            countEven += 1;
+        } else {
+            countOdd += 1;
+        }
+    }
+);
 
 console.log(`
 Количество четных: ${countEven}
